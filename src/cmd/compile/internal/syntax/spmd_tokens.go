a47635af08892b53e4bc7a840661fbf790addf0b
cedric.bail@free.fr <cedric.bail@free.fr>
2025-08-10 16:02:53 -0600

    Make sure we support token parsing depending on runtime experiment flags.

--- src/cmd/compile/internal/syntax/spmd_tokens.go
+++ src/cmd/compile/internal/syntax/spmd_tokens.go
@@ -1,19 +1,28 @@
-//go:build goexperiment.spmd
-
 // Copyright 2025 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 // This file provides token checking utilities for SPMD types.
+// They will only identify SPMD types if the SPMD experiment is enabled at runtime
 
 package syntax
 
+import "internal/buildcfg"
+
 // IsUniformToken reports whether t is the uniform keyword token.
 func IsUniformToken(t token) bool {
+	if !buildcfg.Experiment.SPMD {
+		return false
+	}
+
 	return t == _Uniform
 }
 
 // IsVaryingToken reports whether t is the varying keyword token.
 func IsVaryingToken(t token) bool {
+	if !buildcfg.Experiment.SPMD {
+		return false
+	}
+
 	return t == _Varying
 }
